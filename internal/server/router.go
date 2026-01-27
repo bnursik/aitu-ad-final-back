fdae0198f5a8178f36724e83a464408f76e80e0a
bnursik <nbeusenbek@mail.ru>
2026-01-27 16:58:07 +0500

    feat:added cors

--- internal/server/router.go
+++ internal/server/router.go
@@ -1,12 +1,32 @@
 package server
 
 import (
+	"time"
+
 	"github.com/bnursik/aitu-ad-final-back/internal/app"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func NewRouter(c *app.Container) *gin.Engine {
 	r := gin.New()
+	corsConfig := cors.Config{
+		AllowOrigins: []string{
+			"http://localhost:3000",
+			"https://mangustad.vercel.app/",
+		},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders: []string{
+			"Authorization", "Content-Type",
+		},
+		ExposeHeaders: []string{
+			"Set-Cookie",
+		},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}
+
+	r.Use(cors.New(corsConfig))
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
 
